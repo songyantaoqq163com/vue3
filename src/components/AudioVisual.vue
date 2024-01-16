0ea0af4d27850068fedbf0667de04fe32245a803
songyantao <songyantao@maxwell-gp.com.cn>
2024-01-16 11:26:44 +0800

    音频-

--- src/components/AudioVisual.vue
+++ src/components/AudioVisual.vue
@@ -1,33 +1,39 @@
 <template>
     <div>
         <p>音频1</p>
-        <AVBars
-      caps-color="#FFF"
-      :bar-color="['#f00', '#ff0', '#0f0']"
-      canv-fill-color="#000"
-      :caps-height="2"
-      src="xumi.mp3"
-    ></AVBars>
-    <p>音频2</p>
-        <AVCircle
-      caps-color="#FFF"
-      :bar-color="['#f00', '#ff0', '#0f0']"
-      canv-fill-color="#000"
-      :caps-height="2"
-      src="xumi.mp3"
-    ></AVCircle>
+        <el-upload v-model:file-list="fileList" class="upload-demo"  ref="uploadRef"
+            action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"  :auto-upload="false" >
+            <template #trigger>
+                <el-button type="primary">上传音频</el-button>
+            </template>
+            <template #tip>
+                <div class="el-upload__tip">
+                    jpg/png files with a size less than 500kb
+                </div>
+            </template>
+        </el-upload>
+        <span v-for="item in fileList" :key="item.index">
+            <AVBars caps-color="#FFF" :bar-color="['#f00', '#ff0', '#0f0']" canv-fill-color="#000" :caps-height="2"
+            :src="item.name"></AVBars>
+        </span>
+        <p>音频案例</p>
+        <AVBars caps-color="#FFF" :bar-color="['#f00', '#ff0', '#0f0']" canv-fill-color="#000" :caps-height="2"
+            src="xumi.mp3"></AVBars>
+        <AVCircle caps-color="#FFF" :bar-color="['#f00', '#ff0', '#0f0']" canv-fill-color="#000" :caps-height="2"
+            src="xumi.mp3"></AVCircle>
     </div>
 </template>
 <script>
 import { AVBars } from 'vue-audio-visual'
 import { AVCircle } from 'vue-audio-visual'
 export default {
-    components:{
+    components: {
         AVBars,
         AVCircle
     },
     data() {
         return {
+            fileList: []
         }
     },
     methods: {
