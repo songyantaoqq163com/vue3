2cc73689f22107d07d7d9e1189e57518c6c1df2d
songyantao <songyantao@maxwell-gp.com.cn>
2026-03-12 17:37:14 +0800

    多维数组

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <template>
   <div>
+    <nav class="app_nav">
+      <div class="app_logo_left">首页</div>
+      <ul class='app_nav_ul'>
+        <li>
+          <a>首页<span></span></a>
+        </li>
+        <li>
+          <a>作品 <span></span></a>
+        </li>
+        <li>
+          <a>简介 <span></span></a>
+        </li>
+        <li>设置</li>
+      </ul>
+    </nav>
     <el-row>
       <el-col :span="4" class="borderrightcolor">
-        <el-tree :data="data" :props="defaultProps" accordion @node-click="handleNodeClick">
-        </el-tree>
+
+        <div class="app_left">
+          <ul v-for="item in data" :key="item.index">
+            <span class="cursor">{{item.label}}</span>
+            <li v-for="c in item.children" :key="c.index">
+              <span class="cursor" @click="handleNodeClick(c)"  :class="{'fontRed': c.active}">{{c.label}}</span>
+            </li>
+          </ul>
+        </div>
+        <!-- <el-tree :data="data" :props="defaultProps" accordion @node-click="handleNodeClick">
+        </el-tree> -->
       </el-col>
       <el-col :span="20">
         <router-view></router-view>
@@ -18,6 +42,8 @@ export default {
   name: 'App',
   data() {
     return {
+      selectedItem:null,
+      curTrue:false,
       data: [{
         label: '首页-导航栏',
         router:'/home'
@@ -46,12 +72,18 @@ export default {
           router:'/SecondHover'
         }]
       },{
-        label:'点击事件',
-        router:'/ClickFirst'
-      },{
-        label:'前端风云',
-        router:'/WebStorm'
-      }],
+        label:'其他事件',
+        children:[
+            {
+            label:'点击事件',
+            router:'/ClickFirst'
+          },{
+            label:'前端风云',
+            router:'/WebStorm'
+          }
+        ]
+      }
+    ],
       defaultProps: {
         children: 'children',
         label: 'label'
@@ -61,26 +93,70 @@ export default {
   methods: {
     handleNodeClick(data) {
       if(!data.children){
-        console.log(71,data,this.$router);
+        // this.data.forEach(e=>{
+        //   e.active=false
+        //   if(e.label==data.label){
+        //     e.active=true
+        //   }
+        //   if(e?.children&&e.children.length>0){
+        //     console.log(888,e?.children)
+        //         e?.children.forEach(ee=>{
+        //           ee.active=false
+        //           if(ee.label==data.label){
+        //             ee.active=true
+        //           }
+        //         })
+        //   }
+          
+        // })
+      console.log(2222,this.data)
+        // data['active']=true
+        
+        var data1=this.updateItemRecurs(this.data,data.label)
+        this.data=data1
         this.$router.push(data.router)
       }
+    },
+    updateItemRecurs(array,label){
+      console.log(5,array,label)
+      return array.map(item=>{
+        item.active=false
+        if(item.label==label){
+          return {...item,active:true}
+        }
+        if(item?.children&&item.children.length){
+          return {
+            ...item,
+            children:this.updateItemRecurs(item.children,label)
+          }
+        }
+        return item
+
+      })
     }
   }
 }
 </script>
 
 <style>
+*{
+  margin:0;
+  padding:0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   border:1px solid #2c3e50;
   height: calc(100vh - 20px);
   background:#fff;
-  color:#000;
+  color:#000; */
 }
 audio{
     grid-row: 2!important;
 }
 canvas{
     grid-row:1!important
+}
+.cursor:hover{
+  color: red;
 }
 </style>
